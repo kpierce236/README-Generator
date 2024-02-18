1c78c330f61a2db4f2fa5b2701ca2d7d240ea52e
Kevin Pierce <kpierce236@gmail.com>
2024-02-17 21:14:14 -0500

    added some questions

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:'
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
