3082d0476edc6e60c5e6acc328df0c190dd671b4
Kevin Pierce <kpierce236@gmail.com>
2024-02-19 19:43:32 -0500

    added questions

--- index.js
+++ index.js
@@ -7,15 +7,51 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Provide a description of your project:'
-  },
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide contribution guidelines:'
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:'
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'BSD 3-Clause']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+      }
 ];
 
 // TODO: Create a function to write README file
