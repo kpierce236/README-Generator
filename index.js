d480b0f49b1249577e243c8d9699d98350b7e3e9
Kevin Pierce <kpierce236@gmail.com>
2024-02-19 19:36:53 -0500

    added the init function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generateMarkdown = require('generateMarkdown');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -19,7 +22,17 @@ const questions = [
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+      const readmeContent = generateMarkdown(answers);
+      writeToFile('README.md', readmeContent);
+      console.log('README.md has been generated successfully.');
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
+    });
+}
 
 // Function call to initialize app
 init();
