c82108dfeaec94349f3e9569caf2b4c490568fab
Kevin Pierce <kpierce236@gmail.com>
2024-02-19 19:57:36 -0500

    fixed path

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
-
+const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = [
