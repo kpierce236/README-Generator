920fdc1d3d302ebe58af2a2d687239dd1c037356
Kevin Pierce <kpierce236@gmail.com>
2024-02-19 19:45:17 -0500

    created Markdown fucntion and rendering of README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,63 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // Function to return a license badge based on the license type
+  function renderLicenseBadge(license) {
+    if (license) {
+      return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    } else {
+      return '';
+    }
+  }
+
+  // Function to return the license link based on the license type
+  function renderLicenseLink(license) {
+    if (license) {
+      return `\n* [License](#license)\n`;
+    } else {
+      return '';
+    }
+  }
+
+  // Function to return the license section of the README based on the license type
+  function renderLicenseSection(license) {
+    if (license) {
+      return `## License
+This project is licensed under the ${license} license.`;
+    } else {
+      return '';
+    }
+  }
+
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+${renderLicenseLink(data.license)}
+* [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+${renderLicenseSection(data.license)}
+
+## Questions
+For additional questions, contact ${data.github} at ${data.email}.
 `;
 }
 
