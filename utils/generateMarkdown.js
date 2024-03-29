62cb4bd6926ef5ba9ac971b82107ff2de757df84
Kevin Pierce <kpierce236@gmail.com>
2024-03-28 21:46:36 -0400

    Fixed License and badge rendering

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,47 @@
-function generateMarkdown(data) {
-  // Function to return a license badge based on the license type
-  function renderLicenseBadge(license) {
-    if (license) {
-      return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
-    } else {
-      return '';
-    }
-  }
 
-  // Function to return the license link based on the license type
-  function renderLicenseLink(license) {
-    if (license) {
-      return `\n* [License](#license)\n`;
-    } else {
-      return '';
+
+  function renderLicenseBadge(license) {
+    switch (license) {
+      case 'MIT':
+        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      case 'Apache':
+        return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      case 'GPL':
+        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+      case 'BSD':
+        return '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+      default:
+        return '';
     }
   }
+  
+  function renderLicenseLink(license) {
+    switch (license) {
+      case 'MIT':
+        return 'https://opensource.org/licenses/MIT';
+      case 'Apache':
+        return 'https://opensource.org/licenses/Apache-2.0';
+      case 'GPL':
+        return 'https://www.gnu.org/licenses/gpl-3.0';
+      case 'BSD':
+        return 'https://opensource.org/licenses/BSD-3-Clause';
+      default:
+        return '';
+    }
+  }
+  
 
   // Function to return the license section of the README based on the license type
   function renderLicenseSection(license) {
     if (license) {
       return `## License
-This project is licensed under the ${license} license.`;
+This project is licensed under the [${license}](${renderLicenseLink(license)}) license.`;
     } else {
       return '';
     }
   }
 
+function generateMarkdown(data) {
   return `# ${data.title}
 
 ${renderLicenseBadge(data.license)}
@@ -39,7 +54,7 @@ ${data.description}
 * [Usage](#usage)
 * [Contributing](#contributing)
 * [Tests](#tests)
-${renderLicenseLink(data.license)}
+* [License](#license)
 * [Questions](#questions)
 
 ## Installation
